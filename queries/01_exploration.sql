e97f52a4240a440d472a55c26ca73072e4192f87
Deivid Gabriel <84827844+DGZero@users.noreply.github.com>
2026-07-07 04:07:15 -0300

    Add SQL queries for film and customer statistics

---
+++ queries/01_exploration.sql
@@ -0,0 +1,34 @@
+1 - Quantos filmes existem?
+
+select count(*) AS total_film 
+from film 
+
+Retorno de 1000 filmes
+__________________________________________________________________________________________________________________________
+2 - Quantos clientes existem?
+
+select count(*) AS total_customer 
+from customer 
+
+Retorno de 500 clientes
+_________________________________________________________________________________________________________________________
+3- Quantas locações foram realizadas?
+
+select count(*) AS total_rental
+from rental 
+
+Total de 16.044
+__________________________________________________________________________________________________________________________
+4 - Quantos funcionários existem?
+
+select count(*) AS total_staff
+from staff 
+
+Total de 2 funcionários
+__________________________________________________________________________________________________________________________
+5 -Quantas categorias de filmes existem?
+
+select count(*) AS total_category 
+from category 
+
+Total de 16 categorias
